ccd33d5f2b1d91052a8dc68ed6f65929fcef1880
David Forer <david.j.forer@gmail.com>
2020-10-08 08:07:17 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -8,4 +8,24 @@ class App extends React.Component {
   }
 };
 
+componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(response => response.json())
+    .then (data => {
+      this.setState ({
+        peopleInSpace: data.people
+      })
+    })
+  }
 
+  render () {
+    return (
+      <div>
+        {this.state.peopleInSpace.map(person => person.name)}
+      </div>
+    )
+  }
+
+}
+
+export default App;
